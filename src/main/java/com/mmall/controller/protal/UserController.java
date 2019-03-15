81d354cb6faeb78642ab2cd271f28d3a66d3e514
Awakeyoyoyo <39197200+Awakeyoyoyo@users.noreply.github.com>
2019-03-15 13:15:04 +0800

    the four day working

--- src/main/java/com/mmall/controller/protal/UserController.java
+++ src/main/java/com/mmall/controller/protal/UserController.java
@@ -33,6 +33,52 @@ public class UserController {
             session.setAttribute(Const.CURRENT_USER,response.getData());
         }
         return response;
+    }
 
+    @RequestMapping(value = "logout.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "register.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> register(User user){
+            return  iUserService.register(user);
+    }
+    //校验接口
+    @RequestMapping(value = "check_valid.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str,String type){
+        return iUserService.checkValid(str,type);
+    }
+
+    //获取用户登陆信息
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.GET)
+    @ResponseBody
+    public  ServerResponse<User> getUserInfo(HttpSession session){
+        User user=(User) session.getAttribute(Const.CURRENT_USER);
+        if (user!=null){
+            //serverresponse.data 是一个user
+            return ServerResponse.createBySuccess(user);
+        }
+        return  ServerResponse.createByErrorMessage("用户未登录无法获取用户当前信息");
+    }
+    //忘记问题
+    @RequestMapping(value = "forget_get_question.do",method = RequestMethod.GET)
+    @ResponseBody
+    public  ServerResponse<String> forgetGetQuestion(String username){
+        return iUserService.selectQuestion(username);
+    }
+    //检验问题是否真确
+    @RequestMapping(value = "forget_check_answer.do",method = RequestMethod.GET)
+    @ResponseBody
+    public  ServerResponse<String> forgetCheckAnswer(String username,String question,String answer){
+            return iUserService.checkAnswer(username, question, answer);
+    }
+
+    public ServerResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken){
+        return null;
     }
 }
