ecb9c497f43f7e204c81dfc8671fda3cb79c7ec2
Awakeyoyoyo <39197200+Awakeyoyoyo@users.noreply.github.com>
2019-04-02 21:42:30 +0800

    Nineteenth day's work 1.0

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -26,7 +26,7 @@ public class UserServiceImpl  implements IUserService{
         if (resultCount == 0) {
             return ServerResponse.createByErrorMessage("用户名不存在");
         }
-        //todo 密码登陆MD5
+        //密码登陆MD5
         String md5Password=MD5Util.MD5EncodeUtf8(password);
         User user=userMapper.selectLogin(username,md5Password);
         if (user==null){
